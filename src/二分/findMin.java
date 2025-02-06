a069e1b96bea1772379268f7b5b04c089634029e
PwnDurant <1781389019@qq.com>
2025-02-06 20:28:40 +0800

    优选算法--二分

---
+++ src/二分/findMin.java
@@ -0,0 +1,20 @@
+package 二分;
+
+public class findMin {
+    public static int findMin(int[] nums) {
+        int left=0,right= nums.length-1;
+        int x=nums[right];
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>x) left=mid+1;
+            else right=mid;
+        }
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums={3,4,5,1,2};
+        System.out.println(findMin(nums));
+        
+    }
+}
